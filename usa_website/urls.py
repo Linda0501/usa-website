9c6b6ecae9fd50cf816a5d345fcba642e46200b4
Xurui Chen <xuruichen@berkeley.edu>
2019-10-16 21:47:48 -0700

    right schooling.html (images finally showed up)

--- usa_website/urls.py
+++ usa_website/urls.py
@@ -98,15 +98,8 @@ urlpatterns = [
     url(r'rp/polls$', views.polls, name="polls"),
     url(r'rp/league_of_legends$', views.league_of_legends, name='league_of_legends'),
     url(r'rp/toxic_social_media$', views.toxic_social_media, name='toxic_social_media'),
-<<<<<<< HEAD
     url(r'rp/schooling$', views.schooling, name='schooling'),
     
-=======
-
-    #R+P Spring 2019
-
-
->>>>>>> 43a92bfd417284b40a086683c568ab8ba0e1fd88
     #Education
     url(r'education/installing-r-and-rstudio$', views.r0, name="r0"),
     url(r'education/base-r-and-basic-packages$', views.r1, name="r1"),
