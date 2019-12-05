68dcefddf76a950992bb02f654f3c8eee2c05223
Linda Deng <deng22@berkeley.edu>
2019-12-04 21:20:49 -0800

    Try to pull

--- usa_website/utils/attendance.py
+++ usa_website/utils/attendance.py
@@ -4,11 +4,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("usa_website/utils/creds.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(credentials)
 sheet = client.open("Member_Points").sheet1
 data = sheet.get_all_records()
-
+print(data)
 sid_col = sheet.find("ID").col
 
 
