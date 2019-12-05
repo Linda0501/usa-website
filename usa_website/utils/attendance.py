3100cca4943071a07f0614f81b21c9b6ea84cc9b
Rachel Li <rachelrli@berkeley.edu>
2019-12-04 21:39:00 -0800

    sry for breaking website.

--- usa_website/utils/attendance.py
+++ usa_website/utils/attendance.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("usa_website/utils/creds.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("usa-website/src/usa_website/utils/creds.json", scope)
 http = httplib2.Http()
 http = credentials.authorize(http)
 credentials.refresh(http)
